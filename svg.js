2a9097788b0f32ffb3a94e7cc8b2c94a965f1529
ljm12914 <LJM12914@outlook.com>
2021-02-13 16:16:23 +0800

    make function svg.html

--- svg.js
+++ svg.js
@@ -1,12 +1,16 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8" />
-        <title>trianglify-free</title>
-    </head>
-    <body>
-        <script src="https://cdn.jsdelivr.net/npm/trianglify/dist/trianglify.bundle.min.js"></script>
-        <script src="svg.default.min.js"></script>
-        <script>alert("Right click the photo and choose save to save it.\r\nRefresh to get a new randomly generated photo.");</script>
-    </body>
-</html>
+var tempsvg = document.createElement("svg");
+tempsvg.hidden = true;
+var pattern = trianglify({
+    width: 1920,
+    height: 1080,
+});
+pattern.toSVG(tempsvg);
+var tempa = document.createElement("a");
+const head = "data:text/plain;charset=utf-8,<svg xmlns='http://www.w3.org/2000/svg'";
+var date = new Date();
+var timestamp = date.getFullYear() + "." + date.getMonth() + "." + date.getDay() + "_" + date.getHours() + date.getMinutes() + date.getSeconds();
+tempa.href = head + " width='1920' height='1080'>" + tempsvg.innerHTML + "</svg>";
+tempa.download = "trianglify-free-res-" + timestamp + ".svg";
+document.body.appendChild(tempa);
+tempa.click();
+tempa.remove();
